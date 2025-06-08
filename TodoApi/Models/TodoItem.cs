36ad71b57efac96405ce35d2093a0d94f9dae42e
SantiagoBolumburu <santiagobolumburu@hotmail.com>
2025-06-08 15:01:14 -0300

    Complete las clase de todoItem

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public enum State
+    {
+        todo,
+        doing,
+        donde
+    }
+
+    public long Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required State State { get; set; }
+}
